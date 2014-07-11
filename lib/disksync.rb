babcd754e16a37c970744de45c6f7fc7a918e336
Hermann <herm@Hermann-Fas-MacBook-Pro.local>
2014-07-11 09:40:06 +0200

    Initial version of the main class introduced.

--- lib/disksync.rb
+++ lib/disksync.rb
@@ -1,5 +1,50 @@
-require "disksync/version"
+# require "disksync/version"
 
-module Disksync
+# module Disksync
   # Your code goes here...
+# end
+
+class DiskSynchronizer
+
+    DefaultBasePath = '~/_local_working_copy'
+    DefaultDataSubdirs = [ 'MeineDokumente', '_security' ]
+    DefaultBlobSubdirs = [ 'BLOBs' ]
+    DefaultRsyncOptions = [ '-rtv', '--modify-window=2' ]
+    DefaultDataRsyncOptions = [ '--delete' ]
+    DefaultBlobRsyncOptions = [ '--size-only' ]
+
+    attr_accessor :data_subdirs
+    attr_accessor :blob_subdirs
+    attr_accessor :rsync_options
+
+
+    def initialize()
+        rsync_path = `which rsync`
+    end
+
+    def synchronize_all()
+        if (@data_subdirs.empty?)
+            @data_subdirs = DefaultDataSubdirs.collect {|d| File.directory?(d)
+}
+        @data_subdirs = DefaultDataSubdirs.collect if (@data_subdirs.empty?)
+        @blob_subdirs = DefaultDataSubdirs if (@blob_subdirs.empty?)
+        @data_subdirs.each |dir|
+            synchronize
+        
+
+    end
+
+    def available_default_data_subdirs()
+        @data_subdirs.collect { |d| File.directory?(d) }
+        
+        
+
 end
+
+
+
+
+
+
+
+
